c1e2a0eee45a8d397a5d632d797864c60b354f60
Kartikay <kartikaydwivedi55@gmail.com>
2022-04-02 17:02:46 +0530

    sticky nav added

--- js/script.js
+++ js/script.js
@@ -104,3 +104,24 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle("nav-open");
   });
 });
+
+//sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+observer.observe(sectionHeroEl);
